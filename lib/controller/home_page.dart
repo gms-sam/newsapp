b1d0ea5083089c8a36695b8f054db5df295db49f
gms-sam <gms.sam844@gmail.com>
2021-10-05 20:32:13 +0530

    new

--- lib/controller/home_page.dart
+++ lib/controller/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:get/get.dart';
 import 'package:newsapp/model/categories.dart';
@@ -7,68 +8,86 @@ import 'package:newsapp/model/user_2.dart';
 import 'package:newsapp/services/api_manager.dart';
 
 class HomeController extends GetxController {
+  RxList<RxList<Data>> newsList = <RxList<Data>>[].obs;
+  List<ScrollController> scrollController = <ScrollController>[];
 
-  RxList<RxList<Data>> newsList=<RxList<Data>>[].obs;
-  List<ScrollController> scrollController=<ScrollController>[];
-
-  RxList<NewsModel> news =<NewsModel>[].obs;
-  Rx<User> user = User(email: "",name: "",id:0,createdAt: "",updatedAt: "",emailVerifiedAt: "").obs;
+  RxList<NewsModel> news = <NewsModel>[].obs;
+  Rx<User> user = User(
+          email: "",
+          name: "",
+          id: 0,
+          createdAt: "",
+          updatedAt: "",
+          emailVerifiedAt: "")
+      .obs;
 
   Rx<CategoriesList> categoriesList =
       CategoriesList(data: [], currentPage: -1).obs;
 
-  RxList<int> categories=<int>[].obs;
-  Future<void>getCatogoriesList() async {
+  RxList<int> categories = <int>[].obs;
+  Future<void> getCatogoriesList() async {
     categoriesList.value = (await ApiManager().getCategoryList())!;
-    newsList=RxList.generate(categoriesList.value.data.length, (index) => <Data>[].obs);
-    scrollController=List.generate(categoriesList.value.data.length, (index) => ScrollController());
-    for(int i=0;i<scrollController.length;i++){
+    newsList = RxList.generate(
+        categoriesList.value.data.length, (index) => <Data>[].obs);
+    scrollController = List.generate(
+        categoriesList.value.data.length, (index) => ScrollController());
+    for (int i = 0; i < scrollController.length; i++) {
       scrollController[i].addListener(() {
         listener(i);
       });
     }
-  //  categories.value=newsData.value.data.map((e) => e.categoryId).toSet().toList();
+    //  categories.value=newsData.value.data.map((e) => e.categoryId).toSet().toList();
   }
 
-  void listener(int index)async{
-    if(scrollController[index].position.pixels==scrollController[index].position.maxScrollExtent)
-    {
-    news[index]=await ApiManager().getMoreNews(news[index].nextPageUrl);
-    newsList[index].addAll(news[index].data);
+  RxBool closeTopContainer = false.obs;
+  ScrollController details = ScrollController();
+  void scrollListener() {
+    if (details.position.userScrollDirection == ScrollDirection.reverse) {
+      closeTopContainer.value = true;
+    } else {
+      closeTopContainer.value = false;
     }
   }
 
-  String getCategoryName({required int categoryId}){
-    return categoriesList.value.data.firstWhere((element) => element.id==categoryId).name;
-     }
+  void listener(int index) async {
+    if (scrollController[index].position.pixels ==
+        scrollController[index].position.maxScrollExtent) {
+      news[index] = await ApiManager().getMoreNews(news[index].nextPageUrl);
+      newsList[index].addAll(news[index].data);
+    }
+  }
+
+  String getCategoryName({required int categoryId}) {
+    return categoriesList.value.data
+        .firstWhere((element) => element.id == categoryId)
+        .name;
+  }
 
   Future<User> getUserData() async {
-   String? userData = await FlutterSecureStorage().read(key: "user");
-   if(userData == null){
-     throw "error";
-   }
-   else{
-     User user = User.fromJson(userData);
-     this.user.value = user;
-     return user;
-   }
+    String? userData = await FlutterSecureStorage().read(key: "user");
+    if (userData == null) {
+      throw "error";
+    } else {
+      User user = User.fromJson(userData);
+      this.user.value = user;
+      return user;
+    }
   }
-  
-
-  
 
   @override
   void onInit() {
     super.onInit();
-    getCatogoriesList().then((value)async{
-      for(int i=0;i<categoriesList.value.data.length;i++){
-      NewsModel newsModel = await ApiManager().getNews(categoriesList.value.data[i].name);
-      newsList[i].addAll(newsModel.data);
-      news.add(newsModel);
+    getCatogoriesList().then((value) async {
+      for (int i = 0; i < categoriesList.value.data.length; i++) {
+        NewsModel newsModel =
+            await ApiManager().getNews(categoriesList.value.data[i].name);
+        newsList[i].addAll(newsModel.data);
+        news.add(newsModel);
       }
-      categoriesList.value.data.forEach((element)async {
-      });
+      categoriesList.value.data.forEach((element) async {});
     });
     getUserData();
+    details.addListener(scrollListener);
   }
+  
 }
--- lib/view/news_list.dart
+++ lib/view/news_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:get/get_core/src/get_main.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/intl.dart';
@@ -7,8 +8,16 @@ import 'package:newsapp/model/news_model.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:get/instance_manager.dart';
 
-class NewsListDetails extends StatelessWidget {
+class NewsListDetails extends StatefulWidget {
+  final Data dataList;
+  final int i;
+  NewsListDetails({required this.dataList, required this.i});
 
+  @override
+  State<NewsListDetails> createState() => _NewsListDetailsState();
+}
+
+class _NewsListDetailsState extends State<NewsListDetails> {
   String getCategoryName(int categoryId) {
     switch (categoryId) {
       case 1:
@@ -31,15 +40,16 @@ class NewsListDetails extends StatelessWidget {
   }
 
 
+  bool closeTopContainer = false;
 
-  final Data dataList;
-  final int i;
-  NewsListDetails({required this.dataList, required this.i});
-
-
-   var outPutFormate = DateFormat('dd/MMMM/yyyy ');
   final HomeController homeController = Get.put(HomeController());
 
+  @override
+  void initState() {
+    // TODO: implement initState
+  }
+late double containerHeight = MediaQuery.of(context).size.height * 0.7;
+  
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -51,14 +61,17 @@ class NewsListDetails extends StatelessWidget {
               //height: double.infinity,
               ),
           Container(
-              child: Image.network(
-            dataList.image,
-            errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace){
-                                                  return Image.network("https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png");
-                                                },
-            fit: BoxFit.cover,
-            height: MediaQuery.of(context).size.height * 0.4,
-          )),
+            child: Image.network(
+              widget.dataList.image,
+              errorBuilder: (BuildContext context, Object exception,
+                  StackTrace? stackTrace) {
+                return Image.network(
+                    "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png");
+              },
+              fit: BoxFit.cover,
+              height: MediaQuery.of(context).size.height * 0.4,
+            ),
+          ),
           // Positioned(
           //   bottom: 600,
           //   left: 20,
@@ -71,118 +84,156 @@ class NewsListDetails extends StatelessWidget {
           //         color: Colors.white,
           //         fontSize: 25,
           //         fontWeight: FontWeight.bold),
-          //       ) 
+          //       )
           //   ),
           // ),
           Positioned(
             bottom: 0,
-            child: Container(
+            child: AnimatedContainer(
+              duration: Duration(microseconds: 1000),
               decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.only(
                       topLeft: Radius.circular(35),
                       topRight: Radius.circular(35))),
-              height: MediaQuery.of(context).size.height * 0.7,
+              height:containerHeight,
               width: MediaQuery.of(context).size.width,
               child: Padding(
+              key: ValueKey(closeTopContainer),
                 padding:
                     const EdgeInsets.symmetric(horizontal: 20, vertical: 30),
                 child: SingleChildScrollView(
                   physics: AlwaysScrollableScrollPhysics(),
+                        controller: homeController.details,
+
                   child: Column(
                     children: [
                       //Text(dataList.),
-                      Text(
-              dataList.heading,
-              //maxLines: 3,
-                style: GoogleFonts.dmSans(
-                  textStyle: TextStyle(
-                  //color: Colors.white,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold),
-                ) 
-            ),
-            
-            Row(children: [
-              Container(
-                decoration: BoxDecoration(borderRadius: BorderRadius.circular(15),
-                 color: Colors.blueAccent,
-                ),
-               
-                margin: EdgeInsets.only(right: 10,top: 10),
-                padding: EdgeInsets.symmetric(horizontal: 10,vertical: 5),
-                child: Text(getCategoryName(dataList.categoryId))),
-              Text("Published on : ${outPutFormate
-                                  .format(DateTime.parse(
-                                      DateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z")
-                                          .parse(dataList.updatedAt)
-                                          .toString()))
-                                }"
-                                  ),
-            Text("At  ${
-                      DateFormat.Hms().format(DateTime.parse(dataList.updatedAt))
-                                  }"
-                                  ),
-              
-            ],),
-            Align(
-              alignment: Alignment.bottomRight,
-              child: dataList.author!=null?Text("Auther : ${dataList.author.toString()}"):Text("Auther : None"),
-            ),
-            SizedBox(height:10),
-                      Align(
-                        alignment: Alignment.bottomLeft,
-                        child: Text(
-                          "Description",
-                           style: GoogleFonts.dmSans(textStyle: TextStyle(color: Colors.blueAccent,fontSize: 20, fontWeight: FontWeight.bold,),)
+                      Text(widget.dataList.heading,
+                          //maxLines: 3,
+                          style: GoogleFonts.dmSans(
+                            textStyle: TextStyle(
+                                //color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold),
+                          )),
+
+                      Padding(
+                        padding: const EdgeInsets.only(right: 10, top: 10),
+                        child: Row(
+                          children: [
+                            Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(15),
+                                  color: Colors.blueAccent,
+                                ),
+                                margin: EdgeInsets.only(right: 10),
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 10, vertical: 5),
+                                child: Text(getCategoryName(
+                                    widget.dataList.categoryId))),
+                            Text(
+                              DateFormat('EEEE, MMM dd yyyy - hh:mm a ')
+                                  .format(
+                                      DateTime.parse(widget.dataList.updatedAt))
+                                  .toString(),
+                              style: GoogleFonts.dmSans(),
+                            ),
+                            Text("IST"),
+                          ],
                         ),
                       ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Text(
-                        dataList.content,
-                        style: GoogleFonts.dmSans(textStyle: TextStyle(fontSize: 18)),
-                      ),
+
+                      // SizedBox(height:10),
+                      //           Align(
+                      //             alignment: Alignment.bottomLeft,
+                      //             child: Text(
+                      //               "Description",
+                      //                style: GoogleFonts.dmSans(textStyle: TextStyle(color: Colors.blueAccent,fontSize: 20, fontWeight: FontWeight.bold,),)
+                      //             ),
+                      //           ),
+
+                      //           SizedBox(
+                      //             width: 10,
+                      //           ),
+                      //            Align(
+                      //   alignment: Alignment.bottomLeft,
+                      //   child: dataList.author!=null?Text("Auther : ${dataList.author.toString()}",style: GoogleFonts.dmSans(color: Colors.grey),):Text("Auther : None",style: GoogleFonts.dmSans(color: Colors.grey)),
+                      // ),
+
                       SizedBox(
                         height: 10,
                       ),
                       Align(
-                        alignment: Alignment.bottomRight,
-                            child: TextButton(
+                        alignment: Alignment.bottomLeft,
+                        child: Text("Description",
+                            style: GoogleFonts.dmSans(
+                              textStyle: TextStyle(
+                                color: Colors.blueAccent,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            )),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Text(
+                        widget.dataList.content,
+                        style: GoogleFonts.dmSans(
+                            textStyle: TextStyle(fontSize: 18)),
+                      ),
+                      SizedBox(
+                        height: 10,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          widget.dataList.author != null
+                              ? Text(
+                                  "Auther : ${widget.dataList.author.toString()}",
+                                  style: GoogleFonts.dmSans(color: Colors.grey),
+                                )
+                              : Text("Auther : None",
+                                  style:
+                                      GoogleFonts.dmSans(color: Colors.grey)),
+                          TextButton(
                               onPressed: () {
-                              if(dataList.source!=null){
-                                _launchUrl(dataList.source.toString());
-                              }
-                              else{
-                                _launchUrl("www.google.com");
-                              }
-                            },
-                              child: Text("See More",
-                              style: GoogleFonts.dmSans(textStyle: TextStyle(color: Colors.white)),
+                                if (widget.dataList.source != null) {
+                                  _launchUrl(widget.dataList.source.toString());
+                                } else {
+                                  _launchUrl("www.google.com");
+                                }
+                              },
+                              child: Text(
+                                "See More",
+                                style: GoogleFonts.dmSans(
+                                    textStyle: TextStyle(color: Colors.white)),
                               ),
                               style: ButtonStyle(
-                                backgroundColor: MaterialStateProperty.all<Color>(Colors.blueAccent)
-                              )
-                            )
+                                  backgroundColor:
+                                      MaterialStateProperty.all<Color>(
+                                          Colors.blueAccent))),
+                        ],
                       ),
+                      Divider(),
                       ListView(
                         physics: NeverScrollableScrollPhysics(),
                         shrinkWrap: true,
-                        children: homeController.news[i].data
+                        children: homeController.news[widget.i].data
                             .map((e) => ListTile(
                                   horizontalTitleGap: 20,
                                   onTap: () {
                                     Navigator.pushReplacement(
                                         context,
                                         MaterialPageRoute(
-                                            builder: (context) => NewsListDetails(
+                                            builder: (context) =>
+                                                NewsListDetails(
                                                   dataList: e,
-                                                  i: i,
+                                                  i: widget.i,
                                                 )));
                                   },
                                   leading: Container(
-                                    
                                     height: 200,
                                     width: 100,
                                     child: ClipRRect(
@@ -192,14 +243,22 @@ class NewsListDetails extends StatelessWidget {
                                                 "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png")
                                             : Image.network(
                                                 e.image,
-                                                errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace){
-                                                  return Image.network("https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png");
+                                                errorBuilder: (BuildContext
+                                                        context,
+                                                    Object exception,
+                                                    StackTrace? stackTrace) {
+                                                  return Image.network(
+                                                      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png");
                                                 },
                                                 fit: BoxFit.cover,
                                               )),
                                   ),
-                                  title: Text(e.heading,style: GoogleFonts.dmSans(),),
-                                  subtitle: Text(getCategoryName(e.categoryId),style: GoogleFonts.dmSans()),
+                                  title: Text(
+                                    e.heading,
+                                    style: GoogleFonts.dmSans(),
+                                  ),
+                                  subtitle: Text(getCategoryName(e.categoryId),
+                                      style: GoogleFonts.dmSans()),
                                 ))
                             .toList(),
                       )
